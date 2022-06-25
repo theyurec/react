a062c4cca3e258a13b37da103822900b26cec93d
Yuriy <the.yurec1@yandex.ru>
2022-06-25 19:52:21 +0300

    slider

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 
-import Card from "./Card/Card";
+
+import Slider from "./Slider";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Card />
+      <Slider />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
