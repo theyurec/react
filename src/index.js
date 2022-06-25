50de6bfc858d4e0574d3c859478b9a06eb9486f6
Yuriy <the.yurec1@yandex.ru>
2022-06-25 19:31:10 +0300

    taskbuttons

--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
-import App3 from "./task3/App3";
-import StarMaker from "./task3/App3";
-import TaskButton from "./task3/TaskButton";
-import Slider from "./task3/Slider";
-import List from "./List/List";
-import ListText from "./List/ListText";
-import Card from "./Card/Card";
+
+import TaskButton from "./TaskButton";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Slider /><hr/>
-      <TaskButton /><hr/>
-      <List/><hr/>
-      <Card />
+      <TaskButton />
   </React.StrictMode>
 );
 
